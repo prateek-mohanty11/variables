fd8023194b0f5972a25221fc71ee07ebf69d0213
prateek-mohanty11 <prateekmohanty@2006gmail.com>
2025-01-27 15:27:44 +0530

    first commit

---
+++ intro.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "prateek@gmail.com"
+var accountPassword = "12345"//dont use var 
+accountCity = "Dhenkanal"//another way to assign value to variable without var and let //prefer not to use
+let accountState
+/*
+earlier time in js there was a problem with var and scope " {} " 
+*/
+
+//accountId =  132321 // not allowed
+
+console.log(accountPassword);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]) //arrange the values of the variables in tabular form
